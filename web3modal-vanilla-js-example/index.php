4488ed8302d06adde332896ad1a0e40d72ab3c02
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-05-18 16:17:52 +0100

    Delete web3modal-vanilla-js-example directory

--- web3modal-vanilla-js-example/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/** Ignore this file. This file is added only to deploy this example project on heroku. 
- * Link of which is as follows:
- * @link https://web3modal-vanillajs.herokuapp.com/
- */
-
-include_once('index.html')
-?>
